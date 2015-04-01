62627319f5a835d7c4791b8e2469ba5eb275a43d
Bing van Moorsel <bvanmoorsel@swis.nl>
2015-04-01 10:26:58 +0200

    first setup

---
+++ src/Bingvanmoorsel/Meeting/PackageServiceProvider.php
@@ -0,0 +1,60 @@
+<?php namespace Bingvanmoorsel\Meeting;
+
+
+
+
+use Illuminate\Routing\Router;
+use VictoryCms\Core\Providers\PackageServiceProvider as ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+    public function boot()
+    {
+        echo 'boot';
+
+        $this->routes(function(Router $router){
+            $router->get('/', function(){
+                
+            });
+        });
+    }
+
+    public function install()
+    {
+        echo 'install';
+    }
+
+    public function update()
+    {
+        echo 'update';
+    }
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
